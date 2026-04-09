4a88fbfba5e38c63c89f1f1af26964976506d32e
muhammadhaffejee-lang <muhammad.haffejee@gmail.com>
2026-04-09 12:45:54 -0400

    Add files via upload

---
+++ src/data/config.js
@@ -0,0 +1,11 @@
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzC4daavpUTsCIHo3WlNd4wCQaWgvQKYmu_r0aYRki5taumDJdNOc84gzrBPOtVFICG/exec";
+
+export const COUNTRY_CENTRES = {
+  "Argentina": ["Once", "Mar Del Plata", "Mendoza", "Quilmes"],
+  "Bolivia": ["Santa Cruz"],
+  "Brazil": ["Bras", "Brasilia", "Foz do Iguaçu", "Santana do Livramento", "Sao Bernardo", "Villa Carrao"],
+  "Paraguay": ["Ciudad del Este"],
+  "South Africa": ["Ext. 1 (Lenasia)", "Ext. 13 (Lenasia)"],
+  "Uruguay": ["Montevideo"],
+  "Venezuela": ["Unare", "El Tigre", "San Jose"],
+};
